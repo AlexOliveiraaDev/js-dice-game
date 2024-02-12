89827410d3f6e8de82d3d7c75c5f2ed8734fa68a
Alex Oliveira <contatoalexandergv@gmail.com>
2024-02-12 00:14:27 -0300

    first playable test

--- jogo.js
+++ jogo.js
@@ -1,17 +1,47 @@
-let contaBancaria = 0;
 let txtResultado = document.getElementById("resultado");
+let txtDinheiro = document.getElementById("dinheiro");
+let contaBancaria = 100.00;
+
+
 
 function GerarRand() {
     return Math.floor(Math.random() * 6) + 1
 }
 
-function jogarDado() {
-    let randNum = GerarRand()
+function jogarDado(mult = 1) {
 
-    txtResultado.textContent = "O numero tirado foi " + randNum
-    
+    let randNum = GerarRand()
+    if (randNum) {
+        txtResultado.textContent = "Win!"
+    }
+
+    else {
+        txtResultado.textContent = "Lose"
+    }
+
+    contaBancaria = calcMoney(contaBancaria, randNum * mult)
+    atualizarTxtValor()
+
 }
 
 function calcPar(num) {
     return num % 2
+}
+
+
+function calcMoney(dinheiro, valor) {
+    if (calcPar(valor)) {
+        return dinheiro -= (valor * 10)
+    }
+    else {
+        return dinheiro += (valor * 10)
+    }
+}
+
+function atualizarTxtValor() {
+    txtDinheiro.textContent = "Saldo atual: R$" + contaBancaria;
+}
+
+function tentarOutraJogada() {
+    jogarDado(2)
 }
